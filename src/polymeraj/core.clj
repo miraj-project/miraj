2904531b155f185753db755a918514f45db75c5c
Gregg Reynolds <reynolds-gregg@norc.org>
2015-10-06 03:09:45 -0500

    add co-ns macro; update components

--- src/polymeraj/core.clj
+++ src/polymeraj/core.clj
@@ -1,6 +1,20 @@
-(ns polymeraj.core)
+(ns polymeraj.core
+  (:require [hiccup.page :refer [html5]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmacro co-ns
+  [ns docstr reqs & body]
+  `(html5
+     [:head
+      [:title ~docstr]
+      [:meta {:charset "utf-8"}]
+      [:meta {:name "viewport",
+              :content
+              "width=device-width, minimum-scale=1.0, initial-scale=1, user-scalable=yes"}]
+      [:meta {:name "mobile-web-app-capable", :content "yes"}]
+      [:meta {:name "apple-mobile-web-app-capable", :content "yes"}]
+      [:script {:src "polymer/webcomponentsjs/webcomponents-lite.js"}]
+      [:link {:rel "import", :href "polymer/paper-button/paper-button.html"}]
+      [:link {:rel "import", :href "polymer/paper-material/paper-material.html"}]
+      ]
+     [:body {:id (str '~ns)} ~@body]))
+
